59b70e0ac3c4f332ab2cde6d7267620eeb07520c
LymarMaryna <marina.web.php@gmail.com>
2024-07-03 02:40:19 +0300

    refactoring

--- RunMe.sh
+++ RunMe.sh
@@ -7,5 +7,6 @@ cd ../
 docker-compose up -d --build
 
 # Iitialize laravel project and migrate database
+sleep 5
 docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan migrate
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
-//
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return '
+<div>
+    <h1>Coin Exchange API</h1>
+    <ul>
+        <li><a href="/api/currencies">GET All Currencies</a></li>
+        <li><a href="/api/currencies/bitcoin">GET Bitcoin Price in USD</a></li>
+        <li><a href="/api/currencies/tether">GET Tether Price in USD</a></li>
+    </ul>
+</div>
+';
+
+});
