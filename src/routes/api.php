9d269ec011b73328218e97200711908e34248ca7
LymarMaryna <marina.web.php@gmail.com>
2024-07-02 03:18:23 +0300

    feat: Add issue-token endpoint

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CurrencyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('currencies', [CurrencyController::class, 'index']);
-Route::get('currencies/{identifier}', [CurrencyController::class, 'show']);
+Route::get('/currencies', [CurrencyController::class, 'index']);
+Route::get('/currencies/{identifier}', [CurrencyController::class, 'show']);
+
+Route::post('/issue-token', [AuthController::class, 'issueToken']);
 
 
